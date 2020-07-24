0b6e1a5b6f5236d4be45acc334e88bc4bc6d64bf
vitaliy_somoniuk <vitaliysomonyuk@gmail.com>
2020-07-24 15:18:27 +0300

    Update lesson 2.php
    Update file

--- lesson 2.php
+++ lesson 2.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ImInvisible
- * Date: 24.07.2020
- * Time: 15:05
- */
+
+$title = "Hey";
+$fruit = "apple";
+$winie = "I'm Winie. I have 2 {$fruit}s";
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>lesson 2</title>
+</head>
+<body>
+
+    <h1><?php echo $title?></h1>
+
+    <p><?php echo $winie?></p>
+
+</body>
+</html>
