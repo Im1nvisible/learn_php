74256b6d4507dbeed8add8115d4229b245bb4cc7
vitaliy_somoniuk <vitaliysomonyuk@gmail.com>
2020-07-25 13:26:54 +0300

    Create lesson 4.php
    Add file

---
+++ lesson 4.php
@@ -0,0 +1,52 @@
+<?php
+
+// echo -5 - 3 * 5;
+// echo 5 / 3;
+// echo 10 % 3;
+// echo 2 ** 3;
+// echo pow(2, 4);
+
+/*$a = 5;
+$b = $a;
+var_dump($a);
+$a = 7;
+var_dump($a);
+var_dump($b);*/
+
+/*$a = 5;
+$b = &$a;
+var_dump($a);
+$b = 7;
+var_dump($a);
+var_dump($b);*/
+
+echo 5 + 9;
+
+$str1 = 'Hello,';
+echo $str1 .= ' world';
+
+var_dump($var);
+$var = '';
+unset($var);
+$var = NULL;
+$var++;
+var_dump($var);
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lesson 4</title>
+</head>
+<body>
+<?php
+
+
+
+?>
+</body>
+</html>
