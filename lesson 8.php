e92708ea671b955f884690c58d4e700113c68aea
vitaliy_somoniuk <vitaliysomonyuk@gmail.com>
2020-07-28 22:23:06 +0300

    Create lesson 8.php
    add file

---
+++ lesson 8.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lesson 8</title>
+</head>
+<body>
+<?php
+
+$arr = ['Ivanov', 'Petrov', 'Sidorov'];
+
+// $arr[3] = 'Pupkin';
+$arr[] = 'Pupkin';
+$arr[] = 'Doe';
+print_r($arr);
+
+$names = [
+    'Ivan' => 'Ivanov',
+    'Petr' => 'Petrov',
+    'Sidor' => 'Sidorov',
+];
+// print_r($names);
+/*$i = 1;
+while( $i < 11 ){
+	echo $i . "<br>";
+	$i++;
+}*/
+
+/*for($i = 1; $i < 11; $i++){
+	echo $i . "<br>";
+}*/
+
+for($i = 0; $i < count($arr); $i++ ){
+    echo $arr[$i] . '<br>';
+}
+
+?>
+</body>
+</html>
