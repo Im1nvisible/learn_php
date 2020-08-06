a1330e0ccf8a9567c634df29b0d73ed849eab5ca
vitaliy_somoniuk <vitaliysomonyuk@gmail.com>
2020-08-06 13:53:34 +0300

    Create 2 files
    Add 2 files

---
+++ lesson 11/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lesson 11</title>
+</head>
+<body>
+<p>Lorem ipsum dolor sit amet.</p>
+
+<?php include'include.php' ?>
+<?php include_once 'include.php' ?>
+<?php print_r($names) ?>
+
+<p>Lorem ipsum dolor sit amet.</p>
+</body>
+</html>
