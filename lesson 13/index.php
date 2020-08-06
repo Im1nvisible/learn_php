a5c4a094a84ad72a95852d3044ccd9ecc0fc462f
vitaliy_somoniuk <vitaliysomonyuk@gmail.com>
2020-08-06 15:11:58 +0300

    Add 3 files

---
+++ lesson 13/index.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'include.php';
+header('Content-Type: text/html; charset=utf-8');
+//header('Location: include.php');
+//header('refresh: 5; url=include.php');
+//exit;
+//die;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--<meta charset="UTF-8">-->
+    <title>Lesson 13</title>
+</head>
+<body>
+
+<?=$test?>
+Hello
+
+</body>
+</html>
