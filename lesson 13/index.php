5c72189c586dbff3138121d82f0c19d1b293cc08
vitaliy_somoniuk <vitaliysomonyuk@gmail.com>
2020-10-30 12:46:49 +0200

    Update index.php
    Update file

--- lesson 13/index.php
+++ lesson 13/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'include.php';
 header('Content-Type: text/html; charset=utf-8');
+header('HTTP/1.0 404 Not Found');
 //header('Location: include.php');
 //header('refresh: 5; url=include.php');
 //exit;
