3866466f212b6d32e0f196bf5aae7536a81fb02a
Vitaliy <vitaliysomonyuk@gmail.com>
2021-02-12 13:37:24 +0200

    add file
    add file.

---
+++ lesson 20/index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lesson 20</title>
+</head>
+<body>
+
+<form method="post" action="">
+    <p>
+        <input type="text" name="name">
+    </p>
+    <p>
+        <textarea name="text" id="" cols="30" rows="10"></textarea>
+    </p>
+    <p>
+        <input type="file" name="file">
+    </p>
+    <p>
+        <button type="submit" name="send" value="test">Send</button>
+    </p>
+</form>
+
+
+
+</body>
+</html>
